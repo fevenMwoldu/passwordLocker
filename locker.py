f024da090841cc4ec55d818c15c900e734f82b81
feven <feven.m.woldu@gmail.com>
2019-06-14 10:32:56 +0300

    created my user class

---
+++ locker.py
@@ -0,0 +1,18 @@
+class User:
+    user_list = [] # Empty user list
+
+    def __init__(self,first_name,last_name,password):
+
+    
+
+        self.first_name = first_name
+        self.last_name = last_name
+        self.password = password
+
+
+    def save_user(self):
+
+        '''
+        save_user method saves user objects into user_list
+        '''
+        User.user_list.append(self)
