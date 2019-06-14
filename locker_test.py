dad322ccb5b8f3b034293271e267e992f27751b3
feven <feven.m.woldu@gmail.com>
2019-06-14 12:06:58 +0300

    created my save credential test method

--- locker_test.py
+++ locker_test.py
@@ -1,5 +1,6 @@
-import unittest
-from locker import User
+import unittest # Importing the unittest module
+from locker import User,Credential #Importing the user and credential classes
+
 class TestUser(unittest.TestCase):
     def setUp(self):
         '''
@@ -41,3 +42,47 @@ class TestUser(unittest.TestCase):
         self.assertEqual(len(User.user_list),2)
 
 #codes here for the credentials part
+class TestCredential(unittest.TestCase):
+    def setUp(self):
+        '''
+        Set up method to run before each test cases for the credential class.
+        '''
+        self.new_credential = Credential("feven","facebook","feven","123abc") # create credential object
+
+    def tearDown(self):
+        '''
+        tearDown method that does clean up after each test case has run.
+        '''
+         
+        Credential.credentials_list=[]
+
+    def test_init(self):
+        '''
+        test_init test case to test if the object is initialized properly for the credential class
+        '''
+
+        self.assertEqual(self.new_credential.user_name,"feven")
+        self.assertEqual(self.new_credential.user_site,"facebook")
+        self.assertEqual(self.new_credential.user_account,"feven")
+        self.assertEqual(self.new_credential.password,"123abc")
+
+    def test_save_credential(self):
+        '''
+        test_save_credential test case to test if the user object is saved into
+         the credenttial list
+        '''
+        self.new_credential.save_credential()
+        self.assertEqual(len(Credential.credentials_list),1)
+
+    def test_save_multiple_credentials(self):
+        '''
+        test_save_multiple_credentials to check if we can save multiple credential
+        objects to our credential_list
+        '''
+        self.new_credential.save_credential()
+        test_credential=Credential("feven","facebook","feven","123abc") #new credential
+        test_credential.save_credential()
+        self.assertEqual(len(Credential.credentials_list),2)
+
+if __name__ == '__main__':
+    unittest.main()
